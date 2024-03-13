4225e71e7d2b9e8e613bdd33277c5590acf3aff1
ubaidalam01 <160783629+ubaidalam01@users.noreply.github.com>
2024-03-13 17:57:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Define the make_shirt function
+function make_shirt(size, message) {
+    console.log("Creating a ".concat(size, " shirt with the message: ").concat(message));
+}
+// Call the function with specific size and message
+make_shirt('medium', 'Hello, World!');
